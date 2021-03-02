b67fc8078a6cd80652b7003991f76beb3ddbbe27
Anshul <xtremus@kronos.localdomain>
2021-03-02 15:21:30 +0530

    Added : Bulma lib

--- src/App.svelte
+++ src/App.svelte
@@ -8,17 +8,24 @@
       clearInterval(interval);
     };
   });
-  export let name:string;
+  export let name: string;
 </script>
 
 <style lang="scss">
-  $primary-color: white;
-  $bg: black;
-  h1 {
-    color: $primary-color;
-    background-color: $bg;
-  }
+  @import "./scss/mystyles.scss";
 </style>
 
+<div class="buttons">
+  <button class="button is-primary">Primary</button>
+  <button class="button is-link">Link</button>
+</div>
+
+<div class="buttons">
+  <button class="button is-info">Info</button>
+  <button class="button is-success">Success</button>
+  <button class="button is-warning">Warning</button>
+  <button class="button is-danger">Danger</button>
+</div>
 <h1>Hello {name}!</h1>
+<br />
 <h1>{count}</h1>
